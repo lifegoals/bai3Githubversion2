94bb2b3bfe9d3e41f680b37c6718cd4509393a72
lifegoals <ts2obsession@gmail.com>
2017-09-12 18:52:14 +0700

    commit change

---
+++ app/src/main/java/lifegoals/com/bai3githubversion2/LoginActivity.java
@@ -0,0 +1,10 @@
+package lifegoals.com.bai3githubversion2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hacke on 12/09/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/lifegoals/com/bai3githubversion2/Student.java
@@ -0,0 +1,8 @@
+package lifegoals.com.bai3githubversion2;
+
+/**
+ * Created by hacke on 12/09/2017.
+ */
+
+public class Student {
+}
